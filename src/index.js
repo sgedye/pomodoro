f2afff497a98d20588e242c4e24b7fe91ac9bb7b
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-19 17:15:16 +0800

    :rewind: Cannot get working with custom 'react-timer-hook'

--- src/index.js
+++ src/index.js
@@ -14,8 +14,12 @@ function App() {
   const [ type, setType] = useState('Session')   /* use setType when timer (timeRemaining === zero) */
   const [ breakLength, setBreakLength ] = useState(5)
   const [ sessionLength, setSessionLength ] = useState(1)
-  const [ interv, setInterv ] = useState()
+  // const [ interv, setInterv ] = useState()
   const [ timeRemaining, setTimeRemaining ] = useState({minutes:1, seconds:0})
+  const [ timerNotStarted, setTimerNotStarted] = useState(true)
+  // const [ timeStamp, setTimeStamp] = useState(new Date())
+  // console.log(new Date(2020, 6, 19, 17, 12, 3), 'bluaaah')
+
 
   // Setting the timer
   const changeTime = id => {
@@ -23,44 +27,61 @@ function App() {
     const [type, change] = id.split('-')
     if (type === 'break' && change === 'increment' && breakLength < 60) {
       setBreakLength(breakLength + 1)
-      // setTimer(breakLength + 1)
     } else if (type === 'break' && change === 'decrement' && breakLength > 0) {
       setBreakLength(breakLength - 1)
-      // setTimer(breakLength - 1)
     } else if (type === 'session' && change === 'increment' && sessionLength < 60) {
       setSessionLength(sessionLength + 1)
-      setTimer(sessionLength + 1)
+      setTimeRemaining({ minutes: sessionLength + 1, seconds: 0 })
     } else if (type === 'session' && change === 'decrement' && sessionLength > 0) {
       setSessionLength(sessionLength - 1)
-      setTimer(sessionLength - 1)
+      setTimeRemaining({ minutes: sessionLength - 1, seconds: 0 })
     }
+    // document.getElementById()
   }
-  const setTimer = setMinutes => {
-    setTimeRemaining({minutes: setMinutes, seconds: 0})
-  }
-
-
   let expiryTimestamp = new Date()
-  expiryTimestamp.setMinutes(expiryTimestamp.getMinutes() + timeRemaining.minutes)
-  expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 1)
-  
+  expiryTimestamp = expiryTimestamp.setMinutes(expiryTimestamp.getMinutes() + timeRemaining.minutes)
+
   const {
     seconds, minutes, isRunning, start, pause, resume, restart
   } = useTimer({ expiryTimestamp, onExpire: () => expired() })
-
-  console.log('isRunning: ', isRunning);  
   
-  const playPause = () => isRunning ? pause() : resume();
+  const playPause = () => {
+    if (timerNotStarted) {
+      console.log('starting Timer')
+      let expiry = new Date()
+      console.log('%c Date: ', 'color: orange; font-weight: bold', expiry)
+      // expiry.setMinutes(expiry.getMinutes() + timeRemaining.minutes)
+      console.log('%c Date: ', 'color: orange; font-weight: bold', expiry)
+
+      // Promise.resolve(setTimeStamp(() => new Date(expiry))).then(() => {
+      //   console.log({timeStamp, expiry})
+      //   console.log()
+        start()
+        setTimerNotStarted(false)       
+      // })
+      // console.log({ timeStamp, expiry }, "2")
+    } else {
+      console.log('play/pause', isRunning)
+      isRunning ? pause() : resume();
+    }
+  }
   
   const expired = () => {
-    console.log("expired", type)
-    type === "Session" ? setType("Break") : setType("Session")
-    console.log(type, 'type')
-    let switchMinutes = type === "Session" ? sessionLength : breakLength
-    setTimeRemaining({ minutes: switchMinutes, seconds: 0 })
+    console.log("%c expired", "font-weight: bold; color: red;", type)
+    type === "Session" ? setType(() => "Break") : setType(() => "Session")
+    console.log("%c expired", "font-weight: bold; color: green;", type)
+    let switchMinutes = (type === "Session") ? sessionLength : breakLength
+    console.log(switchMinutes, 'switchMinutes')
+    setTimeRemaining(() => ({ minutes: switchMinutes, seconds: 0 }))
+    console.log(timeRemaining, 'timeRemaining')
+
+  }
+  if (type === "Break") {
+    console.log("%c newType: ", "font-weight: bold; color: green;", type)
+    console.log("%c newTimeRemaining: ", "font-weight: bold; color: green;", timeRemaining)
   }
 
-
+  console.log('%c Minutes, Seconds', 'font-weigth: bold; color: red;', minutes, seconds)
   // const soundAlarm = () => {
   //   const alarm = new Audio("http://soundbible.com/mp3/Temple%20Bell-SoundBible.com-756181215.mp3")
   //   alarm.play()
@@ -68,7 +89,7 @@ function App() {
   
   // document.onload(() => pause())
 
-  const time = `0${minutes}`.slice(-2) + ':' + `0${seconds}`.slice(-2);
+  const time = `0${timeRemaining.minutes}`.slice(-2) + ':' + `0${timeRemaining.seconds}`.slice(-2);
 
   return (
     <div id="pomodoro" style={{ textAlign: "center" }}>
