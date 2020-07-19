f96ffbdf40fe8ffd33f32a7cee48d7cf11e33fbd
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-19 17:48:56 +0800

    :construction: Updating code... :hammer:

--- src/index.js
+++ src/index.js
@@ -1,40 +1,87 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
+import styled from "styled-components"
 
 import './index.css'
 
 import Timers from './Components/Timers'
-import Countdown from './Components/Countdown'
 import Controls from './Components/Controls'
 
-
 function App() {
-  const [ type, setType] = useState('Session')   /* use setType when timer (timeRemaining === zero) */
-  const [ breakLength, setBreakLength ] = useState(5)
-  const [ sessionLength, setSessionLength ] = useState(1)
-  const [ interv, setInterv ] = useState()
-  const [ timeRemaining, setTimeRemaining ] = useState({minutes:1, seconds:0})
-  const [ isPlaying, setIsPlaying] = useState(false)
+  const [type, setType] = useState("Session")
+  const [breakLength, setBreakLength] = useState(5)
+  const [sessionLength, setSessionLength] = useState(1)
+  const [interv, setInterv] = useState()
+  const [timeRemaining, setTimeRemaining] = useState({ minutes: 1, seconds: 0 })
+  const [secondsLeft, setSecondsLeft] = useState(60)
+  const [isPlaying, setIsPlaying] = useState(false)
+  // const [timerNotStarted, setTimerNotStarted] = useState(true)
+  // const [ timeStamp, setTimeStamp] = useState(new Date())
+
+
+///Use UseEffect to update everytime the count is updated.
+  useEffect(() => {
+    document.getElementById('timer-label').innerText = type
+  }, [type])
+
+  useEffect(() => {
+    const minutes = Math.floor(secondsLeft / 60)
+    const seconds = secondsLeft - minutes * 60
+    const time = `0${minutes}`.slice(-2) + ":" + `0${seconds}`.slice(-2)
+
+    document.getElementById("time-left").innerText = time
+  }, [secondsLeft])
+
+  useEffect(() => {
+    console.log(isPlaying)
+    if (isPlaying) {
+      //run the timer
+      // setTimeRemaining(prevState => ({prevState.minutes, prevState.seconds - 1}))
+    } else {
+      //pause the timer
+    }
+  }, [isPlaying])
+
+
+
+  // Change code to only "deal with" seconds.
+  //
+  //
+  //
+  //    Continue below......
+  //
+  //
+  //
+  // Comment out timeRemaining
+
+
+
 
   // Setting the timer
-  const changeTime = id => {
-    console.log('changeTime', id)
-    const [type, change] = id.split('-')
-    if (type === 'break' && change === 'increment' && breakLength < 60) {
+  const changeTime = (id) => {
+    console.log("changeTime", id)
+    const [type, change] = id.split("-")
+    if (type === "break" && change === "increment" && breakLength < 60) {
       setBreakLength(breakLength + 1)
-      // setTimer(breakLength + 1)
-    } else if (type === 'break' && change === 'decrement' && breakLength > 0) {
+    } else if (type === "break" && change === "decrement" && breakLength > 0) {
       setBreakLength(breakLength - 1)
-      // setTimer(breakLength - 1)
-    } else if (type === 'session' && change === 'increment' && sessionLength < 60) {
+    } else if (
+      type === "session" &&
+      change === "increment" &&
+      sessionLength < 60
+    ) {
       setSessionLength(sessionLength + 1)
-      setTimer(sessionLength + 1)
-    } else if (type === 'session' && change === 'decrement' && sessionLength > 0) {
+      setTimeRemaining({ minutes: sessionLength + 1, seconds: 0 })
+    } else if (
+      type === "session" &&
+      change === "decrement" &&
+      sessionLength > 0
+    ) {
       setSessionLength(sessionLength - 1)
-      setTimer(sessionLength - 1)
+      setTimeRemaining({ minutes: sessionLength - 1, seconds: 0 })
     }
   }
-  const setTimer = setMinutes => setTimeRemaining({minutes: setMinutes, seconds: 0})
+
 
   const startTimer = () => {
     setIsPlaying(true)
@@ -44,45 +91,43 @@ function App() {
   }
   const run = () => {
     // if (isPlaying) {
-      let updatedMinutes = timeRemaining.minutes
-      let updatedSeconds = timeRemaining.seconds
-      console.log("runTimer")
-      if (updatedSeconds === 0) {
-        if (updatedMinutes === 0) {
-          console.log("HEY", updatedMinutes, updatedSeconds)
-          setIsPlaying(false)
-          clearInterval(setInterv)
-          // resetTimer()
-          countdownFinished()
-          // return null
-          // setInterv(clearInterval(interv))
-          // setIsPlaying(false)
-          // soundAlarm()
-          // setTimeout(switchTimer, 5000)
-        } else {
-          updatedMinutes--
-          updatedSeconds += 60
-        }
+    let updatedMinutes = timeRemaining.minutes
+    let updatedSeconds = timeRemaining.seconds
+    // console.log("runTimer")
+    if (updatedSeconds === 0) {
+      if (updatedMinutes === 0) {
+        console.log("HEY", updatedMinutes, updatedSeconds)
+        setIsPlaying(false)
+        clearInterval(setInterv)
+        // resetTimer()
+        countdownFinished()
+        // return null
+        // setInterv(clearInterval(interv))
+        // setIsPlaying(false)
+        // soundAlarm()
+        // setTimeout(switchTimer, 5000)
+      } else {
+        updatedMinutes--
+        updatedSeconds += 60
       }
-      if (updatedSeconds > 0) {
-        updatedSeconds--
-      }
-      setTimeRemaining(prevState => ({
-        minutes: updatedMinutes,
-        seconds: updatedSeconds
-      }))
-      // console.log(timeRemaining, updatedMinutes, updatedSeconds)
+    }
+    if (updatedSeconds > 0) {
+      updatedSeconds--
+    }
+    setTimeRemaining((prevState) => ({
+      minutes: updatedMinutes,
+      seconds: updatedSeconds,
+    }))
+    console.log('setTime updaded?', timeRemaining)
+    // console.log(timeRemaining, updatedMinutes, updatedSeconds)
     // }
   }
+
+
+
   // const soundAlarm = () => {
   //   const alarm = new Audio("http://soundbible.com/mp3/Temple%20Bell-SoundBible.com-756181215.mp3")
   //   alarm.play()
-  // }
-  // const switchTimer = () => {
-  //   type === "Session" ? setType("Break") : setType("Session")
-  //   let switchMinutes = type === "Session" ? sessionLength : breakLength
-  //   setTimeRemaining({ minutes: switchMinutes, seconds: 0 })
-  //   console.log('hi')
   // }
 
   const stopTimer = () => {
@@ -107,19 +152,95 @@ function App() {
     setIsPlaying(false)
     console.log(resetMinutes, timeRemaining, isPlaying)
   }
-  const playPause = () => isPlaying ? stopTimer() : startTimer();
+  const playPause = () => (isPlaying ? stopTimer() : startTimer())
 
   return (
-    <div id="pomodoro" style={{ textAlign: 'center' }} >
+    <div id="pomodoro" style={{ textAlign: "center" }}>
       <h1>Pomodoro</h1>
-      <Timers breakLength={breakLength} sessionLength={sessionLength} handleChange={changeTime} />
-      <Countdown type={type} timeRemaining={timeRemaining} />
-      <Controls isPlaying={isPlaying} handleReset={resetTimer} handlePlayPause={playPause} />
+      <Timers
+        breakLength={breakLength}
+        sessionLength={sessionLength}
+        handleChange={changeTime}
+      />
+      <Countdown>
+        <div id="timer-label">{type}</div>
+        <div id="time-left">00:00</div>
+      </Countdown>{" "}
+      <Controls
+        isPlaying={isPlaying}
+        handleReset={resetTimer}
+        handlePlayPause={playPause}
+      />
       <p>Create by Shaunicles</p>
+      <button onClick={() => setType("ture")}>sdfsldkfj</button>
+      <button onClick={() => setType("flase")}>isdf</button>
     </div>
   )
 }
 
+const Countdown = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  padding: 20px 0;
+  background: yellow;
+  border: 2px solid purple;
+  border-radius: 20px;
+  text-align: center;
+  & > div {
+    font-size: 1.8em;
+    font-weight: bold;
+  }
+`
+
 export default App
 
 ReactDOM.render(<App />, document.getElementById("root"))
+
+
+
+
+
+
+
+/*
+
+  const playPause = () => {
+    if (timerNotStarted) {
+      console.log("starting Timer")
+      let expiry = new Date()
+      console.log("%c Date: ", "color: orange; font-weight: bold", expiry)
+      // expiry.setMinutes(expiry.getMinutes() + timeRemaining.minutes)
+      console.log("%c Date: ", "color: orange; font-weight: bold", expiry)
+
+      // Promise.resolve(setTimeStamp(() => new Date(expiry))).then(() => {
+      //   console.log({timeStamp, expiry})
+      //   console.log()
+      start()
+      setTimerNotStarted(false)
+      // })
+      // console.log({ timeStamp, expiry }, "2")
+    } else {
+      console.log("play/pause", isRunning)
+      isRunning ? pause() : resume()
+    }
+  }
+
+  const expired = () => {
+    console.log("%c expired", "font-weight: bold; color: red;", type)
+    type === "Session" ? setType(() => "Break") : setType(() => "Session")
+    console.log("%c expired", "font-weight: bold; color: green;", type)
+    let switchMinutes = type === "Session" ? sessionLength : breakLength
+    console.log(switchMinutes, "switchMinutes")
+    setTimeRemaining(() => ({ minutes: switchMinutes, seconds: 0 }))
+    console.log(timeRemaining, "timeRemaining")
+  }
+  if (type === "Break") {
+    console.log("%c newType: ", "font-weight: bold; color: green;", type)
+    console.log(
+      "%c newTimeRemaining: ",
+      "font-weight: bold; color: green;",
+      timeRemaining
+    )
+  }
+
+*/
