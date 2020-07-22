fc589fa71b52966422385ba8748833e0cad9a8ce
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-22 16:34:10 +0800

    Old branch (#1)
    * :construction: Updating code... :hammer:
    
    * :zap: Working, but not passing tests
    
    * Updated CSS :sparkles:

--- src/index.js
+++ src/index.js
@@ -1,124 +1,107 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
+import styled from "styled-components"
 
 import './index.css'
 
-import Timers from './Components/Timers'
-import Countdown from './Components/Countdown'
-import Controls from './Components/Controls'
-
+import Timers from './components/Timers'
+import Controls from './components/Controls'
 
 function App() {
-  const [ type, setType] = useState('Session')   /* use setType when timer (timeRemaining === zero) */
-  const [ breakLength, setBreakLength ] = useState(5)
-  const [ sessionLength, setSessionLength ] = useState(1)
-  const [ interv, setInterv ] = useState()
-  const [ timeRemaining, setTimeRemaining ] = useState({minutes:1, seconds:0})
-  const [ isPlaying, setIsPlaying] = useState(false)
+  const timerMax = 3600;
+  const timerMin = 60;
+  const defaultBreak = 300;
+  const defaultSession = 1500;
+  const [type, setType] = useState("Session")
+  const [breakLength, setBreakLength] = useState(defaultBreak)
+  const [sessionLength, setSessionLength] = useState(defaultSession)
+  const [interv, setInterv] = useState()
+  const [secondsLeft, setSecondsLeft] = useState(defaultSession)
+  const [isPlaying, setIsPlaying] = useState(false)
 
-  // Setting the timer
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (secondsLeft === 0) {
+      audioRef.current.play()
+    }
+    if (secondsLeft < 0) {
+      if (type === "Session") {
+        setSecondsLeft(() => breakLength);
+        setType(() => "Break")
+      } else {
+        setSecondsLeft(() => sessionLength)
+        setType(() => "Session")
+      }
+    }
+  }, [secondsLeft, sessionLength, breakLength, type])
+
   const changeTime = id => {
-    console.log('changeTime', id)
-    const [type, change] = id.split('-')
-    if (type === 'break' && change === 'increment' && breakLength < 60) {
-      setBreakLength(breakLength + 1)
-      // setTimer(breakLength + 1)
-    } else if (type === 'break' && change === 'decrement' && breakLength > 0) {
-      setBreakLength(breakLength - 1)
-      // setTimer(breakLength - 1)
-    } else if (type === 'session' && change === 'increment' && sessionLength < 60) {
-      setSessionLength(sessionLength + 1)
-      setTimer(sessionLength + 1)
-    } else if (type === 'session' && change === 'decrement' && sessionLength > 0) {
-      setSessionLength(sessionLength - 1)
-      setTimer(sessionLength - 1)
+    const [type, change] = id.split("-")
+    if (type === "break" && change === "increment" && breakLength < timerMax) {
+      setBreakLength(breakLength => breakLength + 60)
+    } else if (type === "break" && change === "decrement" && breakLength > timerMin) {
+      setBreakLength(breakLength => breakLength - 60)
+    } else if (type === "session" && change === "increment" && sessionLength < timerMax) {
+      setSessionLength(sessionLength => sessionLength + 60)
+      setSecondsLeft(sessionLength => sessionLength + 60)
+    } else if (type === "session" && change === "decrement" && sessionLength > timerMin) {
+      setSessionLength(sessionLength => sessionLength - 60)
+      setSecondsLeft(sessionLength => sessionLength - 60)
     }
   }
-  const setTimer = setMinutes => setTimeRemaining({minutes: setMinutes, seconds: 0})
-
   const startTimer = () => {
-    setIsPlaying(true)
-    console.log("startTimer -- isPlaying: ", isPlaying)
-    // run()
-    setInterv(setInterval(run, 100))
+    setIsPlaying(() => true)
+    setInterv(() => setInterval(run, 1000))
   }
-  const run = () => {
-    // if (isPlaying) {
-      let updatedMinutes = timeRemaining.minutes
-      let updatedSeconds = timeRemaining.seconds
-      console.log("runTimer")
-      if (updatedSeconds === 0) {
-        if (updatedMinutes === 0) {
-          console.log("HEY", updatedMinutes, updatedSeconds)
-          setIsPlaying(false)
-          clearInterval(setInterv)
-          // resetTimer()
-          countdownFinished()
-          // return null
-          // setInterv(clearInterval(interv))
-          // setIsPlaying(false)
-          // soundAlarm()
-          // setTimeout(switchTimer, 5000)
-        } else {
-          updatedMinutes--
-          updatedSeconds += 60
-        }
-      }
-      if (updatedSeconds > 0) {
-        updatedSeconds--
-      }
-      setTimeRemaining(prevState => ({
-        minutes: updatedMinutes,
-        seconds: updatedSeconds
-      }))
-      // console.log(timeRemaining, updatedMinutes, updatedSeconds)
-    // }
-  }
-  // const soundAlarm = () => {
-  //   const alarm = new Audio("http://soundbible.com/mp3/Temple%20Bell-SoundBible.com-756181215.mp3")
-  //   alarm.play()
-  // }
-  // const switchTimer = () => {
-  //   type === "Session" ? setType("Break") : setType("Session")
-  //   let switchMinutes = type === "Session" ? sessionLength : breakLength
-  //   setTimeRemaining({ minutes: switchMinutes, seconds: 0 })
-  //   console.log('hi')
-  // }
-
+  const run = () => setSecondsLeft(secondsLeft => secondsLeft - 1);
   const stopTimer = () => {
     clearInterval(interv)
-    setIsPlaying(false)
-    console.log("pauseTimer -- isPlaying: ", isPlaying)
+    setIsPlaying(() => false)
   }
   const resetTimer = () => {
-    console.log("resetTimer")
-    clearInterval(interv)
-    setType("Session")
-    setTimeRemaining({ minutes: sessionLength, seconds: 0 })
-    setIsPlaying(false)
-    console.log(timeRemaining, isPlaying)
+    audioRef.current.pause();
+    audioRef.current.currentTime = 0;
+    clearInterval(interv);
+    setIsPlaying(() => false);
+    setType(() => "Session");
+    setBreakLength(() => defaultBreak);
+    setSessionLength(() => defaultSession);
+    setSecondsLeft(() => defaultSession);
   }
-  const countdownFinished = () => {
-    console.log("countDownFinished")
-    clearInterval(interv)
-    let resetMinutes = type === "Session" ? sessionLength : breakLength
-    setTimeRemaining({ minutes: resetMinutes, seconds: 0 })
-    type === "Session" ? setType("Break") : setType("Session")
-    setIsPlaying(false)
-    console.log(resetMinutes, timeRemaining, isPlaying)
-  }
-  const playPause = () => isPlaying ? stopTimer() : startTimer();
+  const playPause = () => (isPlaying ? stopTimer() : startTimer())
 
+  const minutes = Math.floor(secondsLeft / 60)
+  const seconds = secondsLeft - minutes * 60
+  const time = `0${minutes}`.slice(-2) + ":" + `0${seconds}`.slice(-2)
+  console.log("%c type|time: ", "color: purple; font-weight: bold;", type, time)
   return (
-    <div id="pomodoro" style={{ textAlign: 'center' }} >
-      <h1>Pomodoro</h1>
-      <Timers breakLength={breakLength} sessionLength={sessionLength} handleChange={changeTime} />
-      <Countdown type={type} timeRemaining={timeRemaining} />
-      <Controls isPlaying={isPlaying} handleReset={resetTimer} handlePlayPause={playPause} />
-      <p>Create by Shaunicles</p>
+    <div id="pomodoro">
+      <Heading>Pomodoro Timer</Heading>
+      <Timers
+        breakLength={breakLength / 60}
+        sessionLength={sessionLength / 60}
+        handleChange={changeTime}
+      />
+      <div id="countdown">
+        <div id="timer-label">{type}</div>
+        <div id="time-left">{time}</div>
+      </div>
+      <Controls
+        isPlaying={isPlaying}
+        handleReset={resetTimer}
+        handlePlayPause={playPause}
+      />
+      <audio id="alarm-sound" ref={audioRef} src="http://soundbible.com/mp3/Temple%20Bell-SoundBible.com-756181215.mp3">
+        Your browser does not support the <code>audio</code> element.
+      </audio>
     </div>
   )
 }
+
+const Heading = styled.h1`
+  text-shadow: -1px -1px 5px white, 1px 1px 1px white;
+`;
 
 export default App
 
